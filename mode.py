c3e274b6aa6e0e5ccbc60442f837edadbf6d41ab
Johannes Millegård <nour.johannes@gmail.com>
2017-09-26 21:32:04 +0200

    Allow multiple results in mode

--- mode.py
+++ mode.py
@@ -2,14 +2,21 @@ from collections import Counter
 
 def calculate_mode(numbers):
     c = Counter(numbers)
-    mode = c.most_common(1)
-    return mode[0][0]
+    numbers_freq = c.most_common()
+    max_count = numbers_freq[0][1]
+
+    modes = []
+    for num in numbers_freq:
+        if num[1] == max_count:
+            modes.append(num[0])
+
+    return modes
 
 if __name__ == '__main__':
     try:
         inp = input('Enter some numbers separated by whitespace: ').split(' ')
         numbers = [int(num) for num in inp]
         mean = calculate_mode(numbers)
-        print('Mode of numbers is {0}'.format(mean))
+        print('Mode(s) of numbers is {0}'.format(mean))
     except ValueError:
         print('Invalid input')
